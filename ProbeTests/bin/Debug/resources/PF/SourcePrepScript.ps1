6241671c8273665c6eba3e6ac2f92822f3243531
ValeryPiniazikITechArt <36072114+ValeryPiniazikITechArt@users.noreply.github.com>
2018-04-18 10:58:16 +0300

    Added excel reader

--- ProbeTests/bin/Debug/resources/PF/SourcePrepScript.ps1
+++ ProbeTests/bin/Debug/resources/PF/SourcePrepScript.ps1
@@ -6,18 +6,25 @@
 	
 	$Session1 = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://ps.outlook.com/powershell -Credential $creds -Authentication Basic -AllowRedirection
 	Import-PSSession $Session1
+	Start-Sleep -Seconds 10
 
 	Import-Module msonline
 	Connect-MsolService -Credential $Creds
 
+Write-Host "Get-PublicFolder"
+
 Get-PublicFolder -Recurse -Identity "\AutomationTests" | Disable-MailPublicFolder -Confirm:$false
+Write-Host "Remove-PublicFolder"
 Remove-PublicFolder \AutomationTests -Recurse -Confirm:$false
+Write-Host "New-PublicFolder"
 New-PublicFolder AutomationTests -Path \
+Write-Host "Add-PublicFolderClientPermission"
 Add-PublicFolderClientPermission -Identity \AutomationTests -AccessRights 'Owner' -User $slogin
+Write-Host "New-PublicFolder"
 New-PublicFolder Test1 -Path \AutomationTests
 
 Start-Sleep -Seconds 10
-
+Write-Host "If Get-PublicFolder"
  If (Get-PublicFolder -Identity "\AutomationTests")
                         {
                          Write-Host ("Public Folder successfully created")
@@ -28,4 +35,5 @@ Start-Sleep -Seconds 10
                         }
 
 #Add-PublicFolderClientPermission -Identity \AutomationTests\Test1 -AccessRights 'Owner' -User $userName
+Write-Host "Remove-PSSession"
 Remove-PSSession $Session1
