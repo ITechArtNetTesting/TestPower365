870abc3a54d2a096a8232854a20c51e10e644e0b
ValeryPiniazikITechArt <36072114+ValeryPiniazikITechArt@users.noreply.github.com>
2018-05-03 15:28:41 +0300

    commit

---
+++ AutomationTests/Framework/Forms/NewProjectWizardForms/DiscoveryForms/HowToMatchUsersForm.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using Product.Framework.Elements;
+using System;
+using System.Collections.Generic;
+
+namespace Product.Framework.Forms.NewProjectWizardForms.DiscoveryForms
+{
+    public class HowToMatchUsersForm : BaseWizardStepForm
+    {
+        string[] ValidAttributes = { "UserPrincipalName", "Mail", "ExtensionAttribute" };
+        private static readonly By TitleLocator = By.XPath("//div[contains(@class, 'wizard-body')]//*[contains(text(), 'How would you like to match')]");
+
+        Element sourceDropDownsAttributes = new Element(By.XPath("//div[contains(@class,'dropdown inline')]//a[contains(@data-bind,'source')]"), "Source attributes");
+
+        Element targetDropDownsAttributes = new Element(By.XPath("//div[contains(@class,'dropdown inline')]//a[contains(@data-bind,'target')]"), "Target attributes"); 
+        
+
+        Button sourceDropDownButton = new Button(By.XPath("//button[child::span[contains(@data-bind,'source')]]"), "Source dropdown button");
+
+        Button targetDropDownButton = new Button(By.XPath("//button[child::span[contains(@data-bind,'target')]]"), "Target dropdown button");
+
+        public HowToMatchUsersForm() : base(TitleLocator, "How to match users form")
+        {
+        }
+
+        public HowToMatchUsersForm(By _TitleLocator, string name) : base(_TitleLocator, name)
+        {
+        }       
+
+        bool InvalidAtrsExistInSourceDropDown()
+        {
+            bool result = false;
+            sourceDropDownButton.Click();
+            foreach (IWebElement dropDown in sourceDropDownsAttributes.GetElements())
+            {
+                if (!ArrayContains(ValidAttributes, dropDown.Text))
+                {                    
+                    result= true;
+                    break;
+                }
+            }
+            return result;
+        }
+
+        bool InvalidAtrsExistInTargetDropDown()
+        {
+            bool result = false;
+            targetDropDownButton.Click();
+            foreach (IWebElement dropDown in targetDropDownsAttributes.GetElements())
+            {
+                if (!ArrayContains(ValidAttributes,dropDown.Text))
+                {
+                    result = true;
+                    break;
+                }
+            }
+            return result;
+        }
+
+        bool ArrayContains(string[] array, string value)
+        {
+            bool result = false;
+
+            foreach (string ele in array)
+            {
+                if (value.Contains(ele))
+                {
+                    result = true;
+                    break;
+                }
+            }
+            return result;
+        }
+
+        public void CheckAttributes()
+        {
+            Assert.IsFalse(InvalidAtrsExistInTargetDropDown());
+            Assert.IsFalse(InvalidAtrsExistInSourceDropDown());
+        }
+    }
+}
