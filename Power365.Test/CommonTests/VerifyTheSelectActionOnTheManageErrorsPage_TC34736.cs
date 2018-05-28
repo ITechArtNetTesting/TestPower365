218c730eaf8e14ef9319777141ff7a3ea3dda18a
ValeryPiniazikITechArt <36072114+ValeryPiniazikITechArt@users.noreply.github.com>
2018-05-28 18:41:06 +0300

    Complite TC

---
+++ Power365.Test/CommonTests/VerifyTheSelectActionOnTheManageErrorsPage_TC34736.cs
@@ -0,0 +1,64 @@
+﻿using BinaryTree.Power365.AutomationFramework;
+using BinaryTree.Power365.AutomationFramework.Pages;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryTree.Power365.Test.CommonTests
+{
+    [TestFixture]
+    public class VerifyTheSelectActionOnTheManageErrorsPage_TC34736 : TestBase
+    {
+        public VerifyTheSelectActionOnTheManageErrorsPage_TC34736() : base() { }
+
+        private ErrorsPage atErrorsPage;
+
+        private void verifyTheSelectActionOnTheManageErrorsPage(string username, string password, string client, string projectName)
+        {
+            atErrorsPage = Automation.Common
+                .SingIn(username, password)
+                .ClientSelect(client)
+                .ProjectSelect(projectName)
+                .GetPage<ProjectDetailsPage>()
+                .Menu
+                .ClickErrors();
+            Assert.IsTrue(atErrorsPage.CheckDismissAndExportAreDisplayed(), "Dismiss ar export are not displayed");
+        }
+
+        private void runTest(string clientName, string projectName)
+        {
+            var client = Automation.Settings.GetByReference<Client>(clientName);
+            var project = client.GetByReference<Project>(projectName);
+            string _client = client.Name;
+            string _username = client.Administrator.Username;
+            string _password = client.Administrator.Password;
+            string _projectName = project.Name;
+
+            verifyTheSelectActionOnTheManageErrorsPage(_username, _password, _client, _projectName);
+        }
+
+        [Test]
+        [Category("Integration")]
+        public void VerifyTheSelectActionOnTheManageErrorsPage_Integration_34736()
+        {
+            runTest("client2", "project2");
+        }
+
+        [Test]
+        [Category("MailWithDiscovery")]
+        public void VerifyTheSelectActionOnTheManageErrorsPage_MD_34736()
+        {
+            runTest("client2", "project1");
+        }
+
+        [Test]        
+        [Category("MailOnly")]
+        public void VerifyTheSelectActionOnTheManageErrorsPage_MO_34736()
+        {
+            runTest("client1", "project1");
+        }
+    }
+}
--- Power365Framework/Pages/ErrorsPage.cs
+++ Power365Framework/Pages/ErrorsPage.cs
@@ -8,10 +8,10 @@ using System.Threading.Tasks;
 
 namespace BinaryTree.Power365.AutomationFramework.Pages
 {
-    public class ErrorsPage: PageBase
+    public class ErrorsPage : PageBase
     {
         private static readonly By _locator = By.XPath("//div[@id='users']//input[contains(@placeholder,'error')]");
-      
+
 
         public Element GroupsTab
         {
@@ -40,10 +40,41 @@ namespace BinaryTree.Power365.AutomationFramework.Pages
             }
         }
 
+        private ButtonElement SelectActionButton
+        {
+            get
+            {
+                return new ButtonElement(By.XPath("//div[@id='users']//button//span[@data-translation='SelectAction']//font[contains(text(),'Select action')]"), WebDriver);
+            }
+        }
+
+
+        private Element DismissAction
+        {
+            get
+            {
+                return new Element(By.XPath("//div[contains(@class,'open')]//ul[@class='dropdown-menu scrollable-dropdown' and contains(@data-bind,'actions')]//font[contains(text(),'Dismiss')]"), WebDriver);
+            }
+        }
+
+        private Element ExportAction
+        {
+            get
+            {
+                return new Element(By.XPath("//div[contains(@class,'open')]//ul[@class='dropdown-menu scrollable-dropdown' and contains(@data-bind,'actions')]//font[contains(text(),'Export')]"), WebDriver);
+            }
+        }
+
+
         private readonly string tabFormat = "//ul[@class='nav nav-tabs m-t-lg']//span[contains(text(),'{0}')]";
 
         public ErrorsPage(IWebDriver webDriver) : base(_locator,webDriver) { }
 
+        public bool CheckDismissAndExportAreDisplayed()
+        {
+            SelectActionButton.Click();
+            return DismissAction.IsVisible() && ExportAction.IsVisible();
+        }
        
     }
 }
