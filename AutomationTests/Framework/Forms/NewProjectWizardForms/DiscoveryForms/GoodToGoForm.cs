d54a3bce1b5c5d98be85edb1158bf0784fbe0a0d
ValeryPiniazikITechArt <36072114+ValeryPiniazikITechArt@users.noreply.github.com>
2018-05-28 15:17:49 +0300

    Added text to assert

--- AutomationTests/Framework/Forms/NewProjectWizardForms/DiscoveryForms/GoodToGoForm.cs
+++ AutomationTests/Framework/Forms/NewProjectWizardForms/DiscoveryForms/GoodToGoForm.cs
@@ -16,7 +16,7 @@ namespace Product.Framework.Forms.NewProjectWizardForms.DiscoveryForms
 
         public void CheckNoWavesDefined()
         {
-            Assert.IsTrue(NoWavesDefinedLabel.IsElementVisible());
+            Assert.IsTrue(NoWavesDefinedLabel.IsElementVisible(),"No Waves Defined in not displayed on the summary page");
         }
 	}
 }
