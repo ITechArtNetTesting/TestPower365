4df1fc461ea7e78e359c2f0cf52c47e02f143c18
ValeryPiniazikITechArt <36072114+ValeryPiniazikITechArt@users.noreply.github.com>
2018-05-30 15:26:38 +0300

    Complite TC27071

--- AutomationTests/Framework/Forms/NewProjectWizardForms/IntegrationForms/DownloadDirSyncForm.cs
+++ AutomationTests/Framework/Forms/NewProjectWizardForms/IntegrationForms/DownloadDirSyncForm.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using OpenQA.Selenium;
+using Product.Framework.Elements;
 
 namespace Product.Framework.Forms.NewProjectWizardForms.IntegrationForms
 {
@@ -13,9 +14,16 @@ namespace Product.Framework.Forms.NewProjectWizardForms.IntegrationForms
         private readonly static By TitleLocator = By.XPath(
             "//*/span[@data-translation='LetsDownloadPower365DirectorySyncLiteApplication']");
 
+        private readonly Button _downloadDirSyncButton = new Button(By.XPath("//a[contains(@data-bind,'download')]//span[contains(@data-translation,'Start')]"), "Button to download DirSync Lite");
+
 
         public DownloadDirSyncForm() : base(TitleLocator, "Download dirsync app form")
         {
+        }
+
+        public  bool SeeDownloadDirSync()
+        {
+            return _downloadDirSyncButton.IsElementVisible();
         }
     }
 }
