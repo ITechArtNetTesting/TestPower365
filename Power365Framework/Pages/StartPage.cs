eb86058ef8c7d91675ffdd24bc5876a54756162f
ValeryPiniazikITechArt <36072114+ValeryPiniazikITechArt@users.noreply.github.com>
2018-05-31 11:44:43 +0300

    Complite tc31098

---
+++ Power365Framework/Pages/StartPage.cs
@@ -0,0 +1,24 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryTree.Power365.AutomationFramework.Pages
+{
+    public class StartPage:PageBase
+    {
+        private static readonly By _locator = By.Id("landingContainer");
+
+        public StartPage(IWebDriver webDriver)
+            : base(_locator, webDriver) { }
+
+        private readonly By _migrateAndIntegrateButton = By.XPath("//div[contains(@class,'power365')]");
+
+        public ProjectListPage ClickMigrateAndIntegrateButton()
+        {
+            return ClickElementBy<ProjectListPage>(_migrateAndIntegrateButton,10,1);
+        }
+    }
+}
