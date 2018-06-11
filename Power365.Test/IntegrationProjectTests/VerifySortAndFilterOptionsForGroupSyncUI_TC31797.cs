7c8b3a5dca1add01849697c633ad7444348d7bc5
ValeryPiniazikITechArt <36072114+ValeryPiniazikITechArt@users.noreply.github.com>
2018-06-11 16:52:08 +0300

    Complite TC31797

---
+++ Power365.Test/IntegrationProjectTests/VerifySortAndFilterOptionsForGroupSyncUI_TC31797.cs
@@ -0,0 +1,52 @@
+﻿using BinaryTree.Power365.AutomationFramework;
+using BinaryTree.Power365.AutomationFramework.Pages;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryTree.Power365.Test.IntegrationProjectTests
+{
+    [TestFixture]
+    class VerifySortAndFilterOptionsForGroupSyncUI_TC31797 : TestBase
+    {
+        [Test]
+        [Category("UI")]
+        [Category("Integration")]
+        public void VerifySortAndFilterOptionsForGroupSyncUI_Integration_31797()
+        {
+            TestRun("client2", "project2");
+        }
+
+        private void TestRun(string clientName, string projectName)
+        {
+            var client = Automation.Settings.GetByReference<Client>(clientName);
+            var _project = client.GetByReference<Project>(projectName);
+            string username = client.Administrator.Username;
+            string password = client.Administrator.Password;
+
+            VerifySortAndFilterOptionsForGroupSyncUI(username, password, client.Name, _project.Name);
+        }
+
+        private void VerifySortAndFilterOptionsForGroupSyncUI(string username,string password,string client,string project)
+        {
+            var groupsPage = Automation.Common
+                .SingIn(username, password)
+                .MigrateAndIntegrateSelect()
+                .ClientSelect(client)
+                .ProjectSelect(project)
+                .ClickEditGroups()
+                .GetPage<GroupsPage>();
+            groupsPage.SelectVisibleGroups();
+            groupsPage.ClickViewSelectedGroups();
+            groupsPage.SortSource();
+            Assert.IsTrue(groupsPage.CheckSortWorkingCorrectly());
+            groupsPage.ClickViewAllGroups();
+            groupsPage.ClickFilter();
+            groupsPage.ChooseFilter("Matched");
+            Assert.IsTrue(groupsPage.CheckAllGroupsStatus("Matched"));
+        }
+    }
+}
---
+++ Power365Framework/Pages/GroupsPage.cs
@@ -0,0 +1,110 @@
+﻿using log4net;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryTree.Power365.AutomationFramework.Pages
+{
+    public class GroupsPage : ActionPageBase
+    {
+        private static readonly By _locator = By.XPath("//span[@data-translation='ViewSelectedGroups']");
+        public GroupsPage(IWebDriver webDriver) : base(LogManager.GetLogger(typeof(GroupsPage)), _locator, webDriver)
+        {
+            visibleGroups = new List<String>();
+            WaitForLoadComplete();
+            foreach (var group in webDriver.FindElements(allGroups))
+            {
+                visibleGroups.Add(group.Text);
+            }
+
+        }
+
+        private IList<String> visibleGroups;
+
+        private readonly By SourceSortButton = By.XPath("//span[@data-translation='Source']");        
+
+        private readonly By viewSelectedGroups = By.XPath("//span[@data-translation='ViewSelectedGroups']");
+
+        private readonly By viewAllGroups = By.XPath("//*[text()='View all groups']");
+
+        private readonly By allGroups = By.XPath("//tbody//td[2]/span");
+
+        private readonly By allGroupsStatuses = By.XPath("//tbody//td[4]/span");
+
+        private readonly By allSelectedGroups = By.XPath("//div[@class='ibox-content']//tbody//td[1]/span");
+
+        private readonly By filter = By.XPath("//a[@data-translation='Filter']");
+
+        private readonly string filters = "//div[@class='filter-list']//*[text()='{0}']";
+
+        public void SortSource()
+        {
+            ClickElementBy(SourceSortButton);
+        }
+
+        public void SelectVisibleGroups()
+        {
+            foreach (var group in WebDriver.FindElements(allGroups))
+            {
+                group.Click();
+            }            
+        }
+
+        public void ClickViewSelectedGroups()
+        {
+            ClickElementBy(viewSelectedGroups);
+        }
+
+        public void ClickViewAllGroups()
+        {
+            ClickElementBy(viewAllGroups);
+        }
+
+        public void ChooseFilter(string filter)
+        {
+            var _filter = By.XPath(string.Format(filters, filter));
+            ClickElementBy(_filter);
+        }
+
+        public void ClickFilter()
+        {
+            ClickElementBy(filter);
+            WaitForLoadComplete();
+        }
+
+        public bool CheckSortWorkingCorrectly()
+        {
+            bool result = true;
+            WaitForLoadComplete();            
+            var sortedGroups = visibleGroups.Reverse().ToList();
+            var gottenGroups = WebDriver.FindElements(allSelectedGroups);
+            if (gottenGroups.Count == sortedGroups.Count)
+            {
+                for (int i = 0; i < sortedGroups.Count; i++)
+                {
+                    if (sortedGroups[i] != gottenGroups[i].Text)
+                    {
+                        result = false;
+                        break;
+                    }
+                }
+            }
+            else { result = false; }
+            return result;
+        }
+
+        public bool CheckAllGroupsStatus(string status)
+        {
+            WaitForLoadComplete();
+            bool result = true;
+            foreach (var _status in WebDriver.FindElements(allGroupsStatuses))
+            {
+                if (_status.Text != status) { result = false;break; }
+            }
+            return result;
+        }
+    }
+}
